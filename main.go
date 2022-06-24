2deab5e0f7e3eb4cd5d76f7b650e07ebc845d1a5
soWatt <swattyk@gmail.com>
2022-06-24 08:03:04 -0600

    checking io functionality

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello World!\n")
+		io.WriteString(w, "Testing\n")
 	}
 	http.HandleFunc("/hello", helloHandler)
 	log.Println("listening for requests at http://localhost:8000/hello")
